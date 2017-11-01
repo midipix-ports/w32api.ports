0e2ba7ed88b177f2b04560244779872ade496ee5
Ørjan Malde <foxyred333@gmail.com>
2017-11-01 02:02:20 +0100

    winver.h, winresrc.h: initial commit

--- include/w32api.ports/winresrc.h
+++ include/w32api.ports/winresrc.h
@@ -1,4 +1,8 @@
 #ifndef _W32API_WINRESRC_H_
 #define _W32API_WINRESRC_H_
 
+#include <winuser.h>
+#include <winnt.h>
+#include <winver.h>
+
 #endif
---
+++ include/w32api.ports/winver.h
@@ -0,0 +1,9 @@
+#ifndef _W32API_WINVER_H_
+#define _W32API_WINVER_H_
+
+#include <windef.h>
+
+#define VS_VERSION_INFO 1
+
+
+#endif
