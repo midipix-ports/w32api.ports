825e82a18817f80328c382eaa24f7e24adf67b6b
midipix <writeonce@midipix.org>
2017-10-31 20:41:17 -0400

    windows.h: initial commit.

---
+++ include/w32api.ports/windows.h
@@ -0,0 +1,16 @@
+#ifndef _W32API_WINDOWS_H_
+#define _W32API_WINDOWS_H_
+
+#include <windef.h>
+#include <winbase.h>
+#include <wtypes.h>
+
+#include <winnt.h>
+#include <winnls.h>
+#include <winerror.h>
+
+#include <wingdi.h>
+#include <winuser.h>
+#include <winspool.h>
+
+#endif
