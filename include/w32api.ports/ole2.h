7400fd6f79321554654f147ea551783778585615
Ørjan Malde <foxyred333@gmail.com>
2017-11-04 16:20:21 +0100

    ole2.h: initial commit.

---
+++ include/w32api.ports/ole2.h
@@ -0,0 +1,28 @@
+#ifndef _W32API_OLE2_H_
+#define _W32API_OLE2_H_
+
+#include <windef.h>
+
+
+typedef UINT __stdcall w32api_register_clipboard_format_ansi(
+	LPCSTR);
+
+typedef UINT __stdcall w32api_register_clipboard_format_utf8(
+	LPCSTR);
+
+typedef UINT __stdcall w32api_register_clipboard_format_utf16(
+	LPCWSTR);
+
+typedef int32_t __stdcall w32api_register_drag_drop(
+	HWND,
+	LPDROPTARGET);
+
+
+__dllimport w32api_register_clipboard_format_ansi	RegisterClipboardFormatA;
+__dllimport w32api_register_clipboard_format_utf8	RegisterClipboardFormatM;
+__dllimport w32api_register_clipboard_format_utf16	RegisterClipboardFormatW;
+
+__dllimport w32api_register_drag_drop			RegisterDragDrop;
+
+
+#endif
