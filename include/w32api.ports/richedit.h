e01f5aeba59e948828e87f6b4155ad8e0effafa5
midipix <writeonce@midipix.org>
2017-11-04 13:34:32 -0400

    richedit.h: #define CF_RTF in a compiler- and framework-independent manner.

--- include/w32api.ports/richedit.h
+++ include/w32api.ports/richedit.h
@@ -1,6 +1,8 @@
 #ifndef _W32API_RICHEDIT_H_
 #define _W32API_RICHEDIT_H_
 
-#define CF_RTF TEXT ("Rich Text Format")
+#include <stdint.h>
+
+#define CF_RTF		(uint16_t[]){'R','i','c','h',' ','T','e','x','t',' ','F','o','r','m','a','t',0}
 
 #endif
