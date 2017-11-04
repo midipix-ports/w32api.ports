af4c84f91b99f956f69c1a3351d7ed6791ae1ece
midipix <writeonce@midipix.org>
2017-11-04 18:48:34 -0400

    winuser.h: added the HOOKPROC typedef and window hook constant definitions.

--- include/w32api.ports/winuser.h
+++ include/w32api.ports/winuser.h
@@ -9,6 +9,30 @@
 
 #include <msgbox.h>
 
+typedef LRESULT (__stdcall *HOOKPROC)(int,WPARAM,LPARAM);
+
+#define WH_MSGFILTER		(-1)
+#define WH_JOURNALRECORD	(0)
+#define WH_JOURNALPLAYBACK	(1)
+#define WH_KEYBOARD		(2)
+#define WH_GETMESSAGE		(3)
+#define WH_CALLWNDPROC		(4)
+#define WH_CBT			(5)
+#define WH_SYSMSGFILTER		(6)
+#define WH_MOUSE		(7)
+#define WH_HARDWARE		(8)
+#define WH_DEBUG		(9)
+#define WH_SHELL		(10)
+#define WH_FOREGROUNDIDLE	(11)
+#define WH_CALLWNDPROCRET	(12)
+#define WH_KEYBOARD_LL		(13)
+#define WH_MOUSE_LL		(14)
+
+#define WH_MIN			(-1)
+#define WH_MAX			WH_MOUSE_LL
+#define WH_MINHOOK		WH_MIN
+#define WH_MAXHOOK		WH_MAX
+
 struct user_monitor_info {
 	DWORD	cbSize;
 	RECT	rcMonitor;
