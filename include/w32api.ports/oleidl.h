dcb5a61bc09593a735b24673c736d7c6a38e8042
midipix <writeonce@midipix.org>
2017-11-05 01:22:36 -0400

    oleidl.h: ;provided additional interfaces.

--- include/w32api.ports/oleidl.h
+++ include/w32api.ports/oleidl.h
@@ -1,13 +1,35 @@
 #ifndef _W32API_OLEIDL_H_
 #define _W32API_OLEIDL_H_
 
+#include <stdint.h>
+
 #include <windef.h>
 #include <winerror.h>
 #include <winnt.h>
+
+#include "basetyps.h"
+#include "objfwd.h"
+#include "olectlid.h"
 
 #define DROPEFFECT_NONE 			(0)
 #define DROPEFFECT_COPY 			(1)
 #define DROPEFFECT_MOVE 			(2)
 #define DROPEFFECT_LINK 			(4)
+
+
+/* COM interface: IDropTarget */
+EXTERN_C const IID IID_IDropTarget;
+
+#define INTERFACE  IDropTarget
+DECLARE_INTERFACE_(IDropTarget,IUnknown) {
+        STDMETHOD	(QueryInterface)(THIS_ REFIID,PVOID *) PURE;
+        STDMETHOD_	(ULONG,AddRef)	(THIS) PURE;
+        STDMETHOD_	(ULONG,Release)	(THIS) PURE;
+        STDMETHOD	(DragEnter)	(THIS_ LPDATAOBJECT,DWORD,POINTL,PDWORD) PURE;
+        STDMETHOD	(DragOver)	(THIS_ DWORD,POINTL,PDWORD) PURE;
+        STDMETHOD	(DragLeave)	(THIS) PURE;
+        STDMETHOD	(Drop)		(THIS_ LPDATAOBJECT,DWORD,POINTL,PDWORD) PURE;
+};
+#undef INTERFACE
 
 #endif
