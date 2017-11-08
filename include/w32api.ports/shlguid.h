3fdccb3061ab68a25d20b6919db12e8a48996173
midipix <writeonce@midipix.org>
2017-11-07 20:09:13 -0500

    shlguid.h: initial commit.

---
+++ include/w32api.ports/shlguid.h
@@ -0,0 +1,10 @@
+#ifndef _W32API_SHLGUID_H_
+#define _W32API_SHLGUID_H_
+
+#include <basetyps.h>
+
+#define EXTERN_CONST_GUID(name)	extern const GUID name
+
+EXTERN_CONST_GUID(CLSID_ShellLink);
+
+#endif
