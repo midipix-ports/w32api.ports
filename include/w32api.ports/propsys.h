630ba466142f2f0acf53a95f993c19def1ee780c
midipix <writeonce@midipix.org>
2017-11-07 20:12:37 -0500

    propsys.h: provided additional definitions.

--- include/w32api.ports/propsys.h
+++ include/w32api.ports/propsys.h
@@ -1,8 +1,26 @@
 #ifndef _W32API_PROPSYS_H_
 #define _W32API_PROPSYS_H_
 
+#include <basetyps.h>
+#include <propidl.h>
 
+/* COM interface: IPropertyStore */
+EXTERN_C const IID IID_IPropertyStore;
 
+struct  IPropertyStore;
+typedef struct IPropertyStore	*LPPROPERTYSTORE;
 
+#define INTERFACE  IPropertyStore
+DECLARE_INTERFACE_(IPropertyStore,IUnknown) {
+	STDMETHOD	(QueryInterface)	(THIS_ REFIID,PVOID *) PURE;
+	STDMETHOD_	(ULONG,AddRef)		(THIS) PURE;
+	STDMETHOD_	(ULONG,Release)		(THIS) PURE;
+	STDMETHOD	(GetCount)		(THIS_ DWORD *) PURE;
+	STDMETHOD	(GetAt)			(THIS_ DWORD,PROPERTYKEY *) PURE;
+	STDMETHOD	(GetValue)		(THIS_ REFPROPERTYKEY,PROPVARIANT *) PURE;
+	STDMETHOD	(SetValue)		(THIS_ REFPROPERTYKEY,REFPROPVARIANT) PURE;
+	STDMETHOD	(Commit)		(THIS) PURE;
+};
+#undef INTERFACE
 
 #endif
