ecc5d950f926f1312e704f04cf6f0ff13eb29c4e
midipix <writeonce@midipix.org>
2017-11-08 20:16:16 -0500

    objbase.h: defined STGM_{READ|WRITE|READWRITE}.

--- include/w32api.ports/objbase.h
+++ include/w32api.ports/objbase.h
@@ -5,6 +5,10 @@
 #include <basetyps.h>
 #include <objidl.h>
 
+#define STGM_READ			(0x0)
+#define STGM_WRITE			(0x1)
+#define STGM_READWRITE			(0x2)
+
 typedef int32_t __stdcall w32api_co_create_instance(
 	REFCLSID,
 	LPUNKNOWN,
