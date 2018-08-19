309bf897153fc288cffaac53b1aaf228b23646b5
Ørjan Malde <foxyred333@gmail.com>
2018-08-19 21:27:59 +0200

    winuser.h: add (missing) SW_* defines

--- include/w32api.ports/winuser.h
+++ include/w32api.ports/winuser.h
@@ -379,14 +379,26 @@ typedef w32api_wnd_proc WNDPROC;
 #define CF_HDROP 			15
 #define CF_UNICODETEXT 			13
 
-#define SW_SHOWNORMAL 			1
-#define SW_SHOW 			5
-#define SW_SHOWNOACTIVATE 		4
-#define SW_HIDE 			0
-#define SW_SHOWMAXIMIZED 		3
-#define SW_RESTORE 			9
-#define SW_MAXIMIZE 			3
-#define SW_MINIMIZE 			6
+#define SW_HIDE				0
+#define SW_SHOWNORMAL			1
+#define SW_NORMAL			1
+#define SW_SHOWMINIMIZED		2
+#define SW_SHOWMAXIMIZED		3
+#define SW_MAXIMIZE			3
+#define SW_SHOWNOACTIVATE		4
+#define SW_SHOW				5
+#define SW_MINIMIZE			6
+#define SW_SHOWMINNOACTIVE		7
+#define SW_SHOWNA			8
+#define SW_RESTORE			9
+#define SW_SHOWDEFAULT			10
+#define SW_FORCEMINIMIZE		11
+#define SW_MAX				11
+
+#define SW_PARENTCLOSING		1
+#define SW_OTHERZOOM			2
+#define SW_PARENTOPENING		3
+#define SW_OTHERUNZOOM			4
 
 #define SM_CXSCREEN			0
 #define SM_CYSCREEN			1
