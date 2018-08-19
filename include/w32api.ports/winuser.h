bf3ea8419121cfb7baaa7131a2a51d09c463a806
Ørjan Malde <foxyred333@gmail.com>
2018-08-19 21:00:56 +0200

    winuser.h: fix typo in MapWindowPoints

--- include/w32api.ports/winuser.h
+++ include/w32api.ports/winuser.h
@@ -1579,7 +1579,7 @@ typedef struct w32api_menu_item_info_utf16		MENUITEMINFO;
 
 __dllimport int __stdcall SetCursorPos(int x, int y);
 __dllimport HICON __stdcall CreateIconIndirect(PICONINFO piconinfo);
-__dllimport int _stdcall MapWindowPoints(HWND hWndFrom, HWND hWndTo, LPPOINT lpPoints, UINT cPoints);
+__dllimport int __stdcall MapWindowPoints(HWND hWndFrom, HWND hWndTo, LPPOINT lpPoints, UINT cPoints);
 __dllimport HWND __stdcall GetActiveWindow(void);
 __dllimport HWND __stdcall GetForegroundWindow(void);
 __dllimport int __stdcall EnableWindow(HWND hwnd,BOOL enabled);
