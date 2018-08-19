9d8d4343dbba0892e3bbf0d7ac3ad3127676529e
Ørjan Malde <foxyred333@gmail.com>
2018-08-19 13:52:11 +0200

    shellapi.h: add AB{M,S,E} defines

--- include/w32api.ports/shellapi.h
+++ include/w32api.ports/shellapi.h
@@ -7,4 +7,24 @@ __dllimport UINT __stdcall ExtractIconExW(LPCWSTR lpszFile, int nIconIndex, HICO
 __dllimport UINT __stdcall DragQueryFileW(HDROP hDrop, UINT iFile, LPWSTR lpszFile, UINT cch);
 __dllimport HINSTANCE __stdcall ShellExecuteW(HWND hWnd, LPCWSTR lpOperation, LPCWSTR pFile, LPCWSTR lpParameters, LPCWSTR lpDirectory, INT nShowCmd);
 
+#define ABM_NEW					0x00000000
+#define ABM_REMOVE				0x00000001
+#define ABM_QUERYPOS				0x00000002
+#define ABM_SETPOS				0x00000003
+#define ABM_GETSTATE				0x00000004
+#define ABM_GETTASKBARPOS			0x00000005
+#define ABM_ACTIVATE				0x00000006
+#define ABM_GETAUTOHIDEBAR			0x00000007
+#define ABM_SETAUTOHIDEBAR			0x00000008
+#define ABM_WINDOWPOSCHANGED			0x0000009
+#define ABM_SETSTATE				0x0000000a
+
+#define ABS_AUTOHIDE				0x0000001
+#define ABS_ALWAYSONTOP				0x0000002
+
+#define ABE_LEFT				0
+#define ABE_TOP					1
+#define ABE_RIGHT				2
+#define ABE_BOTTOM				3
+
 #endif
