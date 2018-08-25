301c3b36e0c3ac85f93ca449788403b451b51e8f
Ørjan Malde <foxyred333@gmail.com>
2018-08-25 16:10:16 +0200

    sysinfoapi.h: add GetVersionEx function declarations

--- include/w32api.ports/sysinfoapi.h
+++ include/w32api.ports/sysinfoapi.h
@@ -7,24 +7,28 @@
 __dllimport UINT __stdcall GetSystemDirectoryA(LPSTR lpBuffer, UINT uSize);
 __dllimport UINT __stdcall GetSystemDirectoryM(LPSTR lpBuffer, UINT uSize);
 __dllimport UINT __stdcall GetSystemDirectoryW(LPWSTR lpBuffer, UINT uSize);
+__dllimport int __stdcall GetVersionExA(LPOSVERSIONINFOA lpVersionInformation);
+__dllimport int __stdcall GetVersionExM(LPOSVERSIONINFOM lpVersionInformation);
+__dllimport int __stdcall GetVersionExW(LPOSVERSIONINFOW lpVersionInformation);
+
 __dllimport DWORD __stdcall GetTickCount(void);
 
 
 #ifdef WINAPI_ANSI_DEFAULT
 #define GetSystemDirectory GetSystemDirectoryA
-
+#define GetVersionEx GetVersionExA
 
 #endif
 
 #ifdef WINAPI_UTF8_DEFAULT
 #define GetSystemDirectory GetSystemDirectoryM
-
+#define GetVersionEx GetVersionExM
 
 #endif
 
 #ifdef WINAPI_UTF16_DEFAULT
 #define GetSystemDirectory GetSystemDirectoryW
-
+#define GetVersionEx GetVersionExW
 
 #endif
 
