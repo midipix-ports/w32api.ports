0926bc19ffbf2b824399b795eb4241ee17dd0e2b
Ørjan Malde <foxyred333@gmail.com>
2018-09-09 01:43:52 +0200

    shlwapi.h: define {StrStr,strcat,strcpy,strlen}W functions

--- include/w32api.ports/shlwapi.h
+++ include/w32api.ports/shlwapi.h
@@ -1,6 +1,12 @@
 #ifndef _W32API_SHLWAPI_H_
 #define _W32API_SHLWAPI_H_
 
+#include <u16ports/u16ports.h>
 
+
+#define StrStrW u16_wcsstr
+#define strcatW	u16_wcscat
+#define strcpyW u16_wcscpy
+#define strlenW u16_wcslen
 
 #endif
