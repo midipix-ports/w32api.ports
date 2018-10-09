6605cedaa62a3872a9bf196ebd21da65baee853b
Ørjan Malde <foxyred333@gmail.com>
2018-10-09 21:19:35 +0200

    synchapi.h: add CreateEvent function declarations

--- include/w32api.ports/synchapi.h
+++ include/w32api.ports/synchapi.h
@@ -2,9 +2,26 @@
 #define W32API_SYNCHAPI_H_
 
 #include <windef.h>
+#include <minwinbase.h>
 
 __dllimport DWORD __stdcall SleepEx(DWORD dwMilliseconds, int bAlertable);
 __dllimport void __stdcall Sleep(DWORD dwMilliseconds);
+
+__dllimport HANDLE __stdcall CreateEventA(LPSECURITY_ATTRIBUTES lpEventAttributes, int bManualReset, int bInitialState, LPCSTR lpName);
+__dllimport HANDLE __stdcall CreateEventM(LPSECURITY_ATTRIBUTES lpEventAttributes, int bManualReset, int bInitialState, LPCSTR lpName);
+__dllimport HANDLE __stdcall CreateEventW(LPSECURITY_ATTRIBUTES lpEventAttributes, int bManualReset, int bInitialState, LPCWSTR lpName);
+
+#ifdef WINAPI_ANSI_DEFAULT
+#define CreateEvent CreateEventA
+#endif
+
+#ifdef WINAPI_UTF8_DEFAULT
+#define CreateEvent CreateEventM
+#endif
+
+#ifdef WINAPI_UTF16_DEFAULT
+#define CreateEvent CreateEventW
+#endif
 
 
 #endif
