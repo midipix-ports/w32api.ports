a74b00314c6cc718df74aefa3252096bea579192
Ørjan Malde <red@foxi.me>
2019-02-23 09:15:46 +0100

    handleapi.h: change to proper styling.

--- include/w32api.ports/handleapi.h
+++ include/w32api.ports/handleapi.h
@@ -1,13 +1,50 @@
 #ifndef _W32API_HANDLEAPI_H_
 #define _W32API_HANDLEAPI_H_
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 #include <windef.h>
+
+
+typedef INT __stdcall w32api_handleapi_close_handle(
+	HANDLE);
+
+typedef INT __stdcall w32api_handleapi_duplicate_handle(
+	HANDLE,
+	HANDLE,
+	HANDLE,
+	LPHANDLE,
+	DWORD,
+	BOOL,
+	DWORD);
+
+typedef INT __stdcall w32api_handleapi_get_handle_information(
+	HANDLE,
+	LPDWORD);
+
+typedef INT __stdcall w32api_handleapi_set_handle_information(
+	HANDLE,
+	DWORD,
+	DWORD);
+
+
+
+
+__dllimport w32api_handleapi_close_handle						CloseHandle;
+__dllimport w32api_handleapi_duplicate_handle						DuplicateHandle;
+__dllimport w32api_handleapi_get_handle_information					GetHandleInformation;
+__dllimport w32api_handleapi_set_handle_information					SetHandleInformation;
+
+
+
 
 #define INVALID_HANDLE_VALUE ((HANDLE) (LONG_PTR)-1)
 
-__dllimport int __stdcall CloseHandle(HANDLE hObject);
-__dllimport int __stdcall DuplicateHandle(HANDLE hSourceProcessHandle, HANDLE hSourceHandle, HANDLE hTargetProcessHandle, LPHANDLE lpTargetHandle, DWORD dwDesiredAccess, int bInheritHandle, DWORD dwOptions);
-__dllimport int __stdcall GetHandleInformation(HANDLE hObject, LPDWORD lpdwFlags);
-__dllimport int __stdcall SetHandleInformation(HANDLE hObject, DWORD dwMask, DWORD dwFlags);
 
+#ifdef __cplusplus
+}
 #endif
+
+#endif /* _W32API_HANDLEAPI_H_ */
