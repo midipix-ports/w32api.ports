a2390202538ec8b98fb1f22800aaef7347ade22c
Ørjan Malde <red@foxi.me>
2019-02-23 12:44:25 +0100

    minwindef.h: drop contents, include windef.h.

--- include/w32api.ports/minwindef.h
+++ include/w32api.ports/minwindef.h
@@ -1,10 +1,2 @@
-#ifndef _W32API_MINWINDEF_H_
-#define _W32API_MINWINDEF_H_
-
+/* legacy header, use windef.h */
 #include <windef.h>
-
-#define HIWORD(l) ((WORD) ((((DWORD_PTR) (l)) >> 16) & 0xffff))
-#define LOWORD(l) ((WORD) (((DWORD_PTR) (l)) & 0xffff))
-#define MAKELONG(a, b) ((LONG) (((WORD) (((DWORD_PTR) (a)) & 0xffff)) | ((DWORD) ((WORD) (((DWORD_PTR) (b)) & 0xffff))) << 16))
-
-#endif
