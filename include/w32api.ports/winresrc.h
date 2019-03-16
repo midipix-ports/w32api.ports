bd4e34bed14298575cd0b1446db0edaf3be4e433
Ørjan Malde <red@foxi.me>
2019-03-16 08:47:57 +0100

    winresrc.h: use resource header for RC compiler

--- include/w32api.ports/winresrc.h
+++ include/w32api.ports/winresrc.h
@@ -1,7 +1,11 @@
 #ifndef _W32API_WINRESRC_H_
 #define _W32API_WINRESRC_H_
 
+#ifdef RC_INVOKED
+#include <winuser.rh>
+#else
 #include <winuser.h>
+#endif
 #include <winnt.h>
 #include <dlgs.h>
 #include <winver.h>
